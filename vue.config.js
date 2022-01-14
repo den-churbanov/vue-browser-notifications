7bf5da996b68a8f68fa02e83d3ec0639d7adbd00
churbanov <churbanov@o-code.ru>
2022-01-14 19:55:01 +0400

    start configuration, colors.scss and null-styles.scss includes in vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports= {
+  css:{
+    loaderOptions:{
+      sass: {
+        prependData: `
+          @import "./src/assets/styles/colors.scss";
+          @import "./src/assets/styles/null-styles.scss";
+        `
+      }
+    }
+  }
+}
